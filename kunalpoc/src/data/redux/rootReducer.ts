02ead6ea4a130da5ca2a7287782a5eb1efbefe9f
kunald-ovyo <129162542+kunald-ovyo@users.noreply.github.com>
2023-04-12 13:02:41 +0530

    all data domain and presentation filder staructure base

--- kunalpoc/src/data/redux/rootReducer.ts
+++ kunalpoc/src/data/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import userReducer from './user';
+import allAssetsReducer from './allAssets';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  allAssets: allAssetsReducer,
 });
 
 export default rootReducer;
---
+++ kunalpoc/src/domain/customhooks/homeScreenUseCase.ts
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function homeScreenUseCase() {
+  return (
+    <View>
+      <Text><unknown>homeScreenUseCase</Text>
+    </View>
+  );
+}
